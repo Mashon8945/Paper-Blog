e3229e593650b233fde8775bc744c417ce0282dc
Le'Mashon <Leonardlemashon@gmail.com>
2025-01-30 18:29:56 +0300

    static templates

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'pages',
+    'bootstrap',
+
+    # 3rd Party
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -135,3 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap'
