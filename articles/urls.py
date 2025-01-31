80df1569751cb3e0dee558bf465f3b863befc87e
Le'Mashon <Leonardlemashon@gmail.com>
2025-01-31 17:21:24 +0300

    Article templates

--- articles/urls.py
+++ articles/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('new/', ArticleCreateView.as_view(), name='article_new'),
     path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
     path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
-    path('<int:pk>/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
 ]
